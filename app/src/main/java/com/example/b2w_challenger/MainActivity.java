301a47826175453369d33198eede6641c65cebc9
mmarlonsodre1 <marlon.sodre@al.infnet.edu.br>
2020-02-15 18:35:47 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/b2w_challenger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.b2w_challenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
